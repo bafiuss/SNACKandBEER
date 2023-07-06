6ba6ef0ddb2926454cc4bc8217d2c18170d19757
bafiuss <fsessa02@gmail.com>
2023-07-06 09:32:39 +0200

    implementazione servlet catalogo

---
+++ SNACKandBEER/src/main/java/control/CatalogoServlet.java
@@ -0,0 +1,68 @@
+package control;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+import model.bean.*;
+import model.DAO.*;
+
+
+@WebServlet("/Catalogo")
+public class CatalogoServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	private static Logger logger = Logger.getAnonymousLogger();
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		ProdottoDAO pDao = new ProdottoDAO((DataSource) getServletContext().getAttribute("DataSource"));
+		
+		String categoria = request.getParameter("categoria");
+		
+		List<ProdottoBean> prodotti = null; 
+		
+		if(categoria.equals("birra"))
+		{
+			prodotti = pDao.getAllProducts("Birra");
+			request.setAttribute("type", 1);
+			
+		}
+		else if(categoria.equals("snack"))
+		{
+			prodotti = pDao.getAllProducts("Snack");
+			request.setAttribute("type", 2);
+		}
+		else if(categoria.equals("accessori"))
+		{
+			prodotti = pDao.getAllProducts("Accessori");
+			request.setAttribute("type", 3);
+		}
+		
+		request.setAttribute("prodotti", prodotti);
+		
+		RequestDispatcher dispatcher;
+		dispatcher = this.getServletContext().getRequestDispatcher("/catalogo.jsp");
+		dispatcher.forward(request, response);
+
+		
+		
+		
+	}
+
+}
