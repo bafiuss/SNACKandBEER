27a4084a1dfe09c6c2f7b2b2703bc1c1a49c8211
bafiuss <fsessa02@gmail.com>
2023-07-21 10:15:34 +0200

    fix code smells

--- SNACKandBEER/src/main/java/model/dao/OrdineDAO.java
+++ SNACKandBEER/src/main/java/model/dao/OrdineDAO.java
@@ -7,7 +7,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Collection;
 import java.util.LinkedList;
-import java.util.logging.Logger;
 
 import javax.sql.DataSource;
 
